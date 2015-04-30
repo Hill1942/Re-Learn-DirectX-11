ecfb1c36a2ad192ea2f3613cb27b7d31f169ea5f
Hill1942 <ykdacd123@gmail.com>
2015-04-30 14:28:02 +0800

    start to draw box

---
+++ 05_DirectX_Draw/DrawBox/D3DUtil.h
@@ -0,0 +1,39 @@
+#ifndef D3D_UTIL_H
+#define D3D_UTIL_H
+
+
+#include <d3d11.h>
+#include <DirectXColors.h>
+#include <cassert>
+
+#if defined(DEBUG) | defined(_DEBUG)
+#ifndef HR
+#define HR(x, source)													    \
+	{																\
+		HRESULT hr = (x);											\
+		if(FAILED(hr))												\
+		{															\
+			LPWSTR output;                                          \
+			FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM |				\
+						FORMAT_MESSAGE_IGNORE_INSERTS |				\
+						FORMAT_MESSAGE_ALLOCATE_BUFFER,				\
+						NULL,										\
+						hr,											\
+						MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),  \
+						(LPTSTR) &output,							\
+						0,											\
+						NULL);										\
+			MessageBox(NULL, output, source, MB_OK);				\
+		}															\
+	}
+#endif
+#else
+	#ifndef HR
+	#define HR(x, source) (x)
+	#endif
+#endif 
+
+#define ReleaseCOM(x) { if (x) { x->Release(); x = 0;} }
+
+
+#endif
